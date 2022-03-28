f6adbb9deea632843fe369ba24797f9b13f8e119
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-03-28 00:09:50 -0700

    Fix component usage

--- bot/commands/help.js
+++ bot/commands/help.js
@@ -1,4 +1,5 @@
 import Command from '@conflict/beta/commands';
+import { Embed } from '@conflict/beta/components';
 
 export default new Command({
     name: 'help',
@@ -10,12 +11,10 @@ export default new Command({
     execute: async (command, options, utils) => {
         command.view(
             <message>
-                <embeds>
-                    <embed color="#ff3333" image={{ url: "https://www.gitbook.com/cdn-cgi/image/width=32,height=32,fit=contain,dpr=1,format=auto/https%3A%2F%2Ffiles.gitbook.com%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252FqKkjNKHtBhCUHveEGTY2%252Ficon%252F2sgixS2pVqtWMiBvRGg2%252FConflict.png%3Falt%3Dmedia%26token%3D24c0b0f6-f987-46ca-b4a5-8f193d650bcb" }}>
-                        <title>Hello, world!</title>
-                        <description>Welcome to **Conflict**.</description>
-                    </embed>
-                </embeds>
+                <Embed color="#ff3333" image={{ url: "https://conflict.js.org/favicon.png" }}>
+                    <title>Hello, world!</title>
+                    <description>Welcome to **Conflict**.</description>
+                </Embed>
             </message>
         );
     }
